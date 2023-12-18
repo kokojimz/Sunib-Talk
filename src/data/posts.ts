fdc014a0efd95cb45ad8a87f7a370b336f2d8155
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2023-12-18 12:16:20 +0700

    first commit

---
+++ src/data/posts.ts
@@ -0,0 +1,22 @@
+export const posts = [
+    {
+        id: 1,
+        title: "Blog Post 1",
+        username: "john123",
+    },
+    {
+        id: 2,
+        title: "Blog Post 2",
+        username: "john123",
+    },
+    {
+        id: 3,
+        title: "Blog Post 3",
+        username: "john123",
+    },
+    {
+        id: 4,
+        title: "Blog Post 4",
+        username: "john123",
+    },
+];
